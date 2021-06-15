6ed8bdf2445e67623264c760814fb5eea5bb7210
zkaa02 <azkabusiness02@gmail.com>
2021-06-15 21:13:02 +0700

    complete putBook handler

--- src/routes/postBook.js
+++ src/routes/postBook.js
@@ -1,10 +1,43 @@
-// const nanoid = require('nanoid')
+const { customAlphabet } = require('nanoid')
+const books = require('../books')
 
 const route = {
   method: 'GET',
   path: '/books',
-  handler: (request) => {
-    console.log(request.payload)
+  handler: (request, h) => {
+    const body = request.payload
+    const { pageCount, readPage } = body
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    const id = customAlphabet(alphabet, 8)
+    const finished = pageCount === readPage
+    const insertedAt = new Date().toISOString()
+    const updatedAt = insertedAt
+
+    const newBook = { ...body, id, finished, insertedAt, updatedAt }
+    console.log(newBook)
+
+    books.push(newBook)
+
+    const isSucces = books.some((book) => book.id = id)
+
+    if (isSucces) {
+      const response = h.response({
+        status: 'success',
+        message: 'Buku baru berhasil ditambahkan',
+        data: {
+          bookId: id,
+        },
+      })
+      response.code(201)
+      return response
+    }
+    const response = h.response({
+      status: 'fail',
+      message: 'Catatan gagal ditambahkan',
+    })
+    response.code('500')
+    return response
   },
 }
 
